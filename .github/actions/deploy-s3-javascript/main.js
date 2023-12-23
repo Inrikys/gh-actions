32b81866ce7361518fd25683809f42e79321aca7
Inrikys <inrikys@gmail.com>
2023-12-23 10:55:12 -0300

    :bug: fix: wrong path to import dependencies

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,8 @@
 // npm install @actions/core @actions/github @actions/exec
 
-const core = require('actions/core');
-const github = require('actions/github');
-const exec = require('actions/exec');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 function run() {
     core.notice('Hello from my custom JavaScript Action!')
