f733aada864c6590d494a9e209f7f0596918cd58
Inrikys <inrikys@gmail.com>
2023-12-26 20:41:11 -0300

    :bug: fix: fix wrong variable initialization

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -6,9 +6,9 @@ const exec = require('@actions/exec');
 
 function run() {
     // 1- Get some input values set on action.yml
-   const bucket core.getInput('bucket', { require: true})
-   const bucketRegion core.getInput('bucket-region', { require: true})
-   const distFolder core.getInput('dist-folder', { require: true})
+   const bucket = core.getInput('bucket', { require: true})
+   const bucketRegion = core.getInput('bucket-region', { require: true})
+   const distFolder = core.getInput('dist-folder', { require: true})
 
    // 2 - Upload files
    const s3Uri = `s3://${bucket}`
